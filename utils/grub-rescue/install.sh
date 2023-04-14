03e47a7af4fd0bac23a8b9cc226663b3fdd5cba9
sandorex (Thorium) <rzhw3h@gmail.com>
2023-04-14 18:57:12 +0200

    Improving the install script
    Added argument parsing and some error checks

--- utils/grub-rescue/install.sh
+++ utils/grub-rescue/install.sh
@@ -2,25 +2,76 @@
 #
 # install.sh - add bootable iso file as menuentry in grub
 #
-# requires 7z whiptail
+# requires 7zip/p7zip whiptail lblk
 
-# install.sh <iso file path> [<kernel arguments..>]
+KERNEL_ARGS=
+POSITIONAL_ARGS=()
 
-#GRUB_CONFIG_FILE=/etc/grub.d/40_grub-rescue-iso
+while [ $# -gt 0 ]; do
+    case $1 in
+        --label)
+            ARG_LABEL=$2
+            shift 2
+            ;;
+        --rootpath)
+            ARG_ROOTPATH=$2
+            shift 2
+            ;;
+        --UUID)
+            ISO_PARTITION_UUID=$2
+            shift 2
+            ;;
+        --no-default-kargs)
+            NO_DEFAULT_KARGS=1
+            shift
+            ;;
+        --)
+            shift
+            KERNEL_ARGS=$*
+            break
+            ;;
+        --help|-h)
+            HELP=1
+            shift
+            ;;
+        --*|-*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            # save positional arg
+            POSITIONAL_ARGS+=("$1")
+            shift
+            ;;
+    esac
+done
 
-MENU_LABEL="Rescue LiveCD"
+# restore positional parameters
+set -- "${POSITIONAL_ARGS[@]}"
 
-# NOTE: has to be mounted when grub-mkconfig is ran!
-ISO_PATH="/mnt/d/Downloads/debian-live-11.6.0-amd64-gnome+nonfree.iso"
-# ISO_PATH=/boot/iso/bootable.iso
+if [ -z "$1" ] || [ -n "$HELP" ]; then
+    echo "TODO"
+    exit
+fi
 
-if [ ! -f "$ISO_PATH" ]; then
-    echo "File does not exist at path '$ISO_PATH'"
+if [ ! -f "$1" ]; then
+    echo "Invalid file path '$1'"
     exit 1
 fi
 
-iso_dev_device=$(df "$ISO_PATH" | head -n1 | awk '{ print $1 }')
-ISO_PARTITION_UUID=$(lblk -o UUID "$iso_dev_device" | tail -n1)
+ISO_PATH=${1:?}
+ISO_FILENAME=$(basename "$ISO_PATH")
+LABEL=${ARG_LABEL:-$ISO_FILENAME}
+ROOTPATH=${ARG_ROOTPATH:-$ISO_PATH}
+
+list-iso() {
+    # support both official 7zip and the old deprecated p7zip
+    if command -v 7zz; then
+        7zz l "$1"
+    else
+        7z l "$1"
+    fi
+}
 
 menu() {
     title=$1
@@ -28,9 +79,22 @@ menu() {
     whiptail --backtitle "GRUB Rescue ISO Install" --title "$title" --noitem --menu "" 0 0 0 "$@" 3>&1 1>&2 2>&3
 }
 
-# TODO 7zz is official but 7z is legacy, support both i guess?
+if [ ! -f "$ISO_PATH" ]; then
+    echo "File does not exist at path '$ISO_PATH'"
+    exit 1
+fi
 
-files=$(7zz l "$ISO_PATH" | awk 'index($6, "/") { print $6 }')
+if [ -z "$ISO_PARTITION_UUID" ]; then
+    iso_dev_device=$(df "$ISO_PATH" | tail -n1 | awk '{ print $1 }')
+    echo "== $iso_dev_device"
+    ISO_PARTITION_UUID=$(lsblk -o UUID "$iso_dev_device" | tail -n1)
+    if [ -z "$ISO_PARTITION_UUID" ]; then
+        echo "Could not get UUID of the partition, you can try providing it manually using --UUID <uuid>"
+        exit 1
+    fi
+fi
+
+files=$(list-iso "$ISO_PATH" | awk 'index($6, "/") { print $6 }')
 
 # im printing '_' as item for whiptail, seemed the simplest solution
 linux=$(echo "$files" | awk '/linu[xz][^/]*$/ { print $0, "_" }')
@@ -48,22 +112,27 @@ if ! INITRD_PATH=$(menu 'Select initrd file' $initrd); then
     exit 1
 fi
 
-# TODO take code from https://wiki.debian.org/DebianLive/MultibootISO
+# allow disabling kernel_args
+if [ -z "$NO_DEFAULT_KARGS" ]; then
+    KERNEL_ARGS="findiso=\$iso_path boot=live components quiet splash $KERNEL_ARGS"
+fi
+
+# TODO make a menu like here https://wiki.debian.org/DebianLive/MultibootISO
 cat <<EOF
 #!/bin/sh
-echo "'$MENU_LABEL' added to menuentry"
+echo "Added '$ISO_FILENAME' as '$LABEL' to menu"
 exec tail -n +4 \$0
 
 # this file was automatically generated on $(date)
 # file $ISO_PATH
 # partition uuid $ISO_PARTITION_UUID
 
-menuentry "$MENU_LABEL" {
+menuentry "$LABEL" {
   insmod ext2
-  set iso_path="$ISO_PATH"
+  set iso_path="$ROOTPATH"
   search --no-floppy --fs-uuid --set=root $ISO_PARTITION_UUID
   loopback loop (\$root)\$isofile
-  linux (loop)/$LINUX_PATH findiso=\$iso_path boot=live components quiet splash $BOOT_ARGS
+  linux (loop)/$LINUX_PATH $KERNEL_ARGS
   initrd (loop)/$INITRD_PATH
 }
 EOF
